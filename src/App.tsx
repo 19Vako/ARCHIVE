16a979d4a1b9387f6fce67222fdd6ce0a3e2d0ea
19Vako <frymendzordan@gmail.com>
2025-02-28 18:34:37 +0200

    try 1 to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-/* eslint-disable react/jsx-pascal-case */
-import React from 'react';
+
 import "./App.css";
 import { Routes, Route } from 'react-router';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom'; // <--- заменяем на HashRouter
 
 import Manager_log_in from './screens/Manager_log_in';
 import Admin_log_in from './screens/Admin_log_in';
@@ -12,24 +11,23 @@ import StartScreen from './screens/StartScreen';
 import { Provider } from './context/Context';
 import DocumentCard from './screens/DocumentCard';
 
-
 function App() {
   return (
     <div className="AppContainer">
       <Provider>
         <Router>
           <Routes>
-            <Route path='/' element={<StartScreen/>}/>
-            <Route path='/Manager_log_in' element={<Manager_log_in/>}/>
-            <Route path='/Admin_log_in' element={<Admin_log_in/>}/>
-            <Route path='/Manager' element={<Manager/>}/>
-            <Route path='/Admin' element={<Admin/>}/>
-            <Route path='/document/card/:id' element={<DocumentCard/>}/>
+            <Route path='/' element={<StartScreen />} />
+            <Route path='/Manager_log_in' element={<Manager_log_in />} />
+            <Route path='/Admin_log_in' element={<Admin_log_in />} />
+            <Route path='/Manager' element={<Manager />} />
+            <Route path='/Admin' element={<Admin />} />
+            <Route path='/document/card/:id' element={<DocumentCard />} />
           </Routes>
         </Router>
       </Provider>
     </div>
-  )
+  );
 }
 
 export default App;
